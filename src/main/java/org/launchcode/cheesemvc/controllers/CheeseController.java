0e3d75ca54df8469568b4e346666307ae8fbfb8e
Lonny Snyder <lonnysnyder81@gmail.com>
2017-06-07 16:55:01 -0500

    added edit links to existing cheeses, still to write post handler

--- src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -5,6 +5,7 @@ import org.launchcode.cheesemvc.models.Cheese;
 import org.launchcode.cheesemvc.models.CheeseData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -60,4 +61,18 @@ public class CheeseController {
 
         return "redirect:";
     }
+    @RequestMapping(value ="edit/{cheeseId}", method = RequestMethod.GET)
+    public String displayEditForm(Model model, @PathVariable int cheeseId){
+        Cheese editCheese = CheeseData.getById(cheeseId);
+        model.addAttribute("cheese",editCheese);
+        model.addAttribute("title","Edit cheese "+editCheese.getName());
+        return"cheese/edit";
+
+
+    }
+    //@RequestMapping(value = "edit", method =RequestMethod.POST)
+    //public String processEditForm(int cheeseId, String name, String description){
+
+
+    //}
 }
